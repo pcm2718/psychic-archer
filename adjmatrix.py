5aca4ee3de73c856e9176df451ba1a09f8e828c9
Parker Michaelson <parker.michaelson@gmail.com>
2013-09-27 20:57:50 -0600

    Added adjecency matrix functionality and generation.

---
+++ adjmatrix.py
@@ -0,0 +1,43 @@
+class AdjMatrix:
+
+    def __init__(self, nodecount):
+        self.nodecount = nodecount
+
+        self.matrix = []
+
+        for i in range(0, nodecount):
+            for j in range(0, nodecount):
+                self.matrix.append(0)
+
+
+
+    def set_adjvalue(self, m, n, adjvalue):
+        if m >= self.nodecount or n >= self.nodecount:
+            sys.exit("Invalid node access.")
+
+        self.matrix[self.nodecount*m + n] = adjvalue
+        self.matrix[self.nodecount*n + m] = adjvalue
+
+
+
+    def get_adjvalue(self, m, n):
+        if m >= self.nodecount or n >= self.nodecount:
+            sys.exit("Invalid node access.")
+
+        return self.matrix[self.nodecount*n + m]
+
+
+
+    def print_adjmatrix(self):
+        print "\n\n",
+        print "["
+
+        for i in range(0, self.nodecount):
+            for j in range(0, self.nodecount):
+                print '{:>6,.2f}'.format(self.matrix[self.nodecount*i + j]),
+                print ',',
+            print "\n",
+
+        print "]"
+        print "\n\n",
+
